df65ffebd3a5a0700ef31f196be179fec6ccdf4d
Amia <hermionegranger3423@gmail.com>
2025-08-28 16:28:44 +0300

    1. Added a login frame functionality with temporary design. 2. Made session store login more complicated. 3. Added pages specifically for login frame-related logic, which will be ignored in middleware.

--- app/middleware/01.setup.global.ts
+++ app/middleware/01.setup.global.ts
@@ -1,12 +1,14 @@
 import { useSessionStore } from "~/stores/session"
 
 export default defineNuxtRouteMiddleware(async (to, from)=> {
+    if (to.path === '/login-frame' || from.path === '/login-frame') return;
+
     if (import.meta.server) return;
 
     const session = useSessionStore();
 
     if (!session.loaded) {
-        await session.checkIfAuthorized();
+        await session.checkIfAuthorizedGlobally();
         session.markFirstLoad();
     }
 })
