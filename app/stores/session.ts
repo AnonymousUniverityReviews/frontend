7caffdf9c53bbcc024c050caa4ebf410d7e13633
Amia <hermionegranger3423@gmail.com>
2025-08-29 13:30:43 +0300

    Updated session store to now also include some userData (currently, only hard-coded user ID inside GET status endpoint).

--- app/stores/session.ts
+++ app/stores/session.ts
@@ -3,11 +3,16 @@ import { getLoginState } from "~/services/authService";
 export const useSessionStore = defineStore('sessionStore', {
     state: () => ({
         authorized: false,
-        loaded: false
+        loaded: false,
+        userData: {
+            id: 0
+        }
     }),
     actions: {
         async checkIfAuthorizedLocally() {
-            this.authorized = (await getLoginState()).loggedIn;
+            const loginState = await getLoginState();
+            this.authorized = loginState.loggedIn;
+            this.userData = loginState.userData;
             return this.authorized;
         },
         async checkIfAuthorizedGlobally() {
@@ -16,7 +21,9 @@ export const useSessionStore = defineStore('sessionStore', {
              * checkIfAuthorizedLocally will check only for existance of token inside user's cookies,
              * while checkIfAuthorizedGlobally will try to verify token from cookies on back-end side.
              */
-            this.authorized = (await getLoginState()).loggedIn;
+            const loginState = await getLoginState();
+            this.authorized = loginState.loggedIn;
+            this.userData = loginState.userData;
             return this.authorized;
         },
         markFirstLoad() {
--- server/api/auth/status.ts
+++ server/api/auth/status.ts
@@ -2,6 +2,9 @@ export default defineEventHandler(async (event) => {
     const loggedIn = getCookie(event, 'access_token');
 
     return {
-        loggedIn: Boolean(loggedIn)
+        loggedIn: Boolean(loggedIn),
+        userData: {
+            id: 100 // temporary, will be changed in future
+        }
     };
 });
