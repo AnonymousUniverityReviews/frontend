81409ff79ee8b88175601ad90c64914fd189c872
Amia <hermionegranger3423@gmail.com>
2025-09-03 22:00:58 +0300

    No longer authorizing users that are not authorized to access /[object]/[id]/rate pages.

---
+++ app/constants/global.ts
@@ -0,0 +1,1 @@
+export const pageObjects = ["school", "faculty", "professor"];
--- app/middleware/01.setup.global.ts
+++ app/middleware/01.setup.global.ts
@@ -1,4 +1,26 @@
 import { useSessionStore } from "~/stores/session"
+import { pageObjects } from "~/constants/global";
+
+function validateObjectIDRateURL(path: string): boolean {
+    // Regex for URL path in format "/[object]/[id]/rate"
+    const regex = /^\/(\w+)\/(\d+)\/rate$/;
+
+    if (regex.test(path)) {
+        const [, object, id] = regex.exec(path)!;
+
+        const session = useSessionStore();
+        
+        if (!session.authorized) {
+            return false;
+        }
+
+        if (!pageObjects.includes(object!)) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 export default defineNuxtRouteMiddleware(async (to, from)=> {
     if (to.path === '/login-frame' || from.path === '/login-frame') return;
@@ -10,5 +32,9 @@ export default defineNuxtRouteMiddleware(async (to, from)=> {
     if (!session.loaded) {
         await session.checkIfAuthorizedGlobally();
         session.markFirstLoad();
+    }
+
+    if (!validateObjectIDRateURL(to.path)) {
+        return abortNavigation("Not authorized.");
     }
 })
